66f4838b40cd66646ff2295b92f93fc9fe9b1af8
muminoff <smuminov@gmail.com>
2019-07-16 10:20:48 +0900

    Add more test cases

--- tests/test_transliterate.py
+++ tests/test_transliterate.py
@@ -3,7 +3,7 @@ import asynctest
 import sys
 
 sys.path.insert(0, "..")
-import transliterate
+from transliterate.core import lat2cyr
 
 
 class TestLat2CyrTransliteration(asynctest.TestCase):
@@ -14,9 +14,14 @@ class TestLat2CyrTransliteration(asynctest.TestCase):
         self.my_loop.close()
 
     async def test_upper(self):
-        self.assertEqual(await transliterate.lat2cyr("AQSH"), "АҚШ")
+        self.assertEqual(await lat2cyr("AQSh"), "АҚШ")
 
     async def test_e_letter(self):
-        self.assertEqual(await transliterate.lat2cyr("ketayotib"), "кетаётиб")
-        self.assertEqual(await transliterate.lat2cyr("eshak"), "эшак")
-        self.assertEqual(await transliterate.lat2cyr("kemada"), "кемада")
+        self.assertEqual(await lat2cyr("ketayotib"), "кетаётиб")
+        self.assertEqual(await lat2cyr("eshak"), "эшак")
+        self.assertEqual(await lat2cyr("kemada"), "кемада")
+
+    async def test_sentence_with_e_letter(self):
+        sentence = "Eshmat bugun maktabga eshakda ketdi"
+        translit = "Эшмат бугун мактабга эшакда кетди"
+        self.assertEqual(await lat2cyr(sentence), translit)
